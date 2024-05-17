7151d3d8e8494f3072ef96279ca6e407094ff777
Nicolas Le Guiniec <nleguiniec@gmail.com>
2024-05-17 16:52:05 +0200

    add mots et phrases

--- scripts/config.js
+++ scripts/config.js
@@ -1,3 +1,3 @@
 //Liste des mots disponible pour le jeu
-const listeMots = ["Cachalot","Pétunia","Serviette"]
-const listePhrases=["Pas de panique !", "La vie, l’univers et le reste", "Merci pour le poisson"]
+const listeMots = ["Cachalot","Pétunia","Serviette","radiateur","train","gribouiller","fragment","ascendant","bourgeon"]
+const listePhrases=["Pas de panique !", "La vie, l’univers et le reste", "Merci pour le poisson", "Est-ce que le troubadour sombrait ?", "Vous monnayez un hypnotiseur...","Au guichet de la poste, le visiteur se calque sur un fossile.","Nous nous bornons à ruminer.","Un goinfre élève deux maître d'hôtels."]
